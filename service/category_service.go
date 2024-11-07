a811ac5f53d8b0e323d8f53adb5693a026a0feb7
aryo-setyotama <aryosetyotama66@gmail.com>
2024-11-07 07:22:31 +0700

    Initial setup + adding model, repository, and service

---
+++ service/category_service.go
@@ -0,0 +1,16 @@
+package service
+
+import (
+	"context"
+	"latihan_api2/model/web"
+)
+
+type CategoryService interface {
+	create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse
+	Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse
+	Delete(ctx context.Context, categoryId int)
+	FindById(ctx context.Context, categoryId int) web.CategoryResponse
+	FindAll(ctx context.Context) []web.CategoryResponse
+}
+
+// bikin kontrak untuk category interface
---
+++ service/category_service_impl.go
@@ -0,0 +1,4 @@
+package service
+
+type CategoryServiceImpl struct {
+}
